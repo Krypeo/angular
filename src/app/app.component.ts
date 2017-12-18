89359507205ca326808d1f304d213a10b85dde6e
Krypeo <33468148+Krypeo@users.noreply.github.com>
2017-12-18 14:55:04 +0100

    top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { OnInit, OnDestroy, OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent{
+  title = 'Angular';
 }
+
---
+++ src/app/test-lukas/test-lukas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-lukas',
+  templateUrl: './test-lukas.component.html',
+  styleUrls: ['./test-lukas.component.css']
+})
+export class TestLukasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
